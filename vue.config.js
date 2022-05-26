f11e5040d9c6a67a6c5a4ea16c8ab1152aa23b93
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-05-26 16:38:21 +0900

    Modify: change webpack setting for default index.html file

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].template = 'docs/index.html';
+      return args;
+    });
+  },
 });
