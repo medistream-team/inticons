0a7ea5e0284a28c2b556122adbdd9dc039dcf330
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-05-30 10:07:14 +0900

    Add: Add createConfig.js to automate creating config

---
+++ fonts/createConfig.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+(() => {
+  const writeFilePath = 'fonts/test.json';
+  const svgs = 'icons';
+  const files = fs.readdirSync(svgs);
+  const glyphs = [];
+
+  files.forEach((file, index) => {
+    const fileData = fs.readFileSync(`${svgs}/${file}`, 'utf-8');
+    const path = fileData.split('path d="')[1].slice(0, -9);
+
+    glyphs.push({
+      uid: (index + 1).toString(),
+      css: file.slice(0, -4),
+      code: index + 59392,
+      src: 'custom_icons',
+      selected: true,
+      svg: {
+        path,
+        width: 24,
+      },
+      search: [file.slice(0, -4)],
+    });
+  });
+
+  const config = {
+    name: 'inticon',
+    css_prefix_text: 'ii ii-',
+    css_use_suffix: false,
+    hinting: true,
+    units_per_em: 1000,
+    ascent: 850,
+    copyright: 'medistream',
+    glyphs,
+  };
+
+  fs.writeFileSync(writeFilePath, JSON.stringify(config, null, 2));
+})();
