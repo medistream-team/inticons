134474b5feaecab152a550c6bf338ca41f7b10eb
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-06-07 10:47:40 +0900

    Add: Create copied modal and apply scss

--- docs/src/views/CheatSheetView.vue
+++ docs/src/views/CheatSheetView.vue
@@ -29,6 +29,8 @@
         </li>
       </ul>
     </div>
+
+    <div v-if="isIconClicked" class="copied">Copied to clipboard</div>
   </div>
 </template>
 
@@ -40,6 +42,7 @@ export default {
     return {
       icons: config.glyphs,
       usageIcon: config.glyphs[0].css,
+      isIconClicked: false,
     };
   },
   methods: {
@@ -50,7 +53,11 @@ export default {
       );
 
       if (isHexOrName) {
+        this.isIconClicked = true;
         navigator.clipboard.writeText(e.target.innerHTML);
+        setTimeout(() => {
+          this.isIconClicked = false;
+        }, 1500);
       }
     },
     changeUsageIcon(e) {
@@ -60,79 +67,97 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .cheatsheet {
   width: 80%;
   height: 100vh;
   margin: 0 auto;
-}
 
-.usage-icon {
-  padding: 10px;
-  border: 1px solid #ddd;
-  background-color: #f1f1f1;
-}
-
-.usage-note {
-  position: relative;
-  padding: 8px 8px 8px 12px;
-  margin: 16px 0 0;
-}
-
-.usage-note::before {
-  content: ' ';
-  width: 0.25rem;
-  height: 100%;
-  background: #ddd;
-  position: absolute;
-  border-radius: 0.25rem;
-  top: 0;
-  left: 0;
-}
-
-.icons-list {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  padding: 0;
-}
-
-.icons-item {
-  margin-bottom: 8px;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.icons-item::marker {
-  display: none;
-}
-
-.icons-item code,
-.icons-item span {
-  cursor: pointer;
-}
-
-.icons-item code:hover,
-.icons-item span:hover {
-  color: #3c90be;
-}
-
-.icons-item code {
-  display: inline-block;
-  padding: 4px;
-  border: 1px solid #ddd;
-  border-radius: 4px;
-}
-
-@media screen and (min-width: 640px) {
-  .icons-list {
-    grid-template-columns: repeat(3, 1fr);
+  .usage {
+    &-icon {
+      padding: 10px;
+      border: 1px solid #ddd;
+      background-color: #f1f1f1;
+    }
   }
-}
 
-@media screen and (min-width: 1024px) {
-  .icons-list {
-    grid-template-columns: repeat(4, 1fr);
+  &-note {
+    position: relative;
+    padding: 8px 8px 8px 12px;
+    margin: 16px 0 0;
+
+    &::before {
+      content: ' ';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 0.25rem;
+      height: 100%;
+      border-radius: 0.25rem;
+      background-color: #ddd;
+    }
+  }
+
+  .icons {
+    &-list {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      padding: 0;
+    }
+
+    &-item {
+      margin-bottom: 8px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      &::marker {
+        display: none;
+      }
+
+      code,
+      span {
+        cursor: pointer;
+
+        &:hover {
+          color: #3c90be;
+        }
+      }
+
+      code {
+        display: inline-block;
+        padding: 4px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+      }
+    }
+  }
+
+  .copied {
+    position: fixed;
+    top: 100px;
+    left: 50%;
+    width: 200px;
+    padding: 10px 15px;
+    border: 1px solid #3c90be;
+    border-radius: 4px;
+    color: #fff;
+    text-align: center;
+    background-color: #3c90be;
+    transform: translateX(-50%);
+    box-shadow: 0 0 10px rgb(0 0 0 / 20%);
+  }
+
+  @media screen and (min-width: 640px) {
+    .icons-list {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .icons-list {
+      grid-template-columns: repeat(4, 1fr);
+    }
   }
 }
 </style>
